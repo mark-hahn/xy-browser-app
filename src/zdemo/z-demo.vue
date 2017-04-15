5776efec18552fd5250853115d296940a10c4525
Mark Hahn <mark@hahnca.com>
2017-04-15 15:37:23 -0700

    added buttons

--- src/zdemo/z-demo.vue
+++ src/zdemo/z-demo.vue
@@ -1,4 +1,3 @@
-
 <!-- global -->
 <style lang="less">
   .zdemo-page-pane {
@@ -53,6 +52,9 @@
     display: inline-block;
     float:right;
   }
+  .button {
+    margin-left:35px;
+  }
 </style>
 
 <template lang="html">
@@ -63,19 +65,37 @@
         best image with the fewest lines ...
       </div>
       <div>
-        Low: <vue-slider class="slider" v-bind="sliderLow" v-model="lowThresh"></vue-slider>
+        Low: <vue-slider class="slider"
+                         v-bind="sliderLow" v-model="lowThresh"
+                         v-on:callback="lowChange"></vue-slider>
       </div>
       <div>
-        High: <vue-slider class="slider" v-bind="sliderHigh" v-model="highThresh"></vue-slider>
+        High: <vue-slider class="slider"
+                          v-bind="sliderHigh" v-model="highThresh"
+                          v-on:callback="highChange"></vue-slider>
       </div>
     </div>
+
     <div id="right-controls">
-      <div id="undo-label-line">
-        <div id="undo-labl"> Undo<span class="arrow">&#x2190;</span> </div>
-        <div id="redo-labl"> <span class="arrow">&#x2192;</span> Redo</div>
+      <div id="undo" v-if="mode=='editing'">
+        <div id="undo-label-line">
+          <div id="undo-labl"> Undo<span class="arrow">&#x2190;</span> </div>
+          <div id="redo-labl"> <span class="arrow">&#x2192;</span> Redo</div>
+        </div>
+        <vue-slider class="slider"
+                    v-bind="sliderUndo" v-model="undoPos"
+                    v-on:callback="undoChange"></vue-slider>
       </div>
-      <vue-slider class="slider" v-bind="sliderUndo" v-model="undoPos"></vue-slider>
+      <div id="buttons">
+        <button class="button" type="button" id="freezeBtn"
+                v-on:click="freezeClick">{{freezeBtnText}}</button>
+        <button class="button" type="button"
+                v-bind:disabled="mode != 'frozen'">Edit</button>
+        <button class="button" type="button"
+                v-bind:disabled="mode != 'frozen'">Finished</button>
+      </div>
     </div>
+
     <video id="webcam" width="640" height="480" style="display:none;"></video>
     <div style=" width:640px;height:480px;">
         <canvas id="canvas" width="640" height="480"></canvas>
@@ -96,35 +116,43 @@
         lowThresh: 0,
         highThresh: 0,
         undoPos: 0,
+        undoCount: 0,
+        mode: 'capturing',
+        freezeBtnText: "Freeze",
+        video: null,
+        canvas: null,
+        ctx: null,
+        img_u8: null,
+        imageData: null,
         sliderLow: {
-					value: 20, width: 255, height: 8, dotSize: 20, min: 0, max: 255, interval: 1,
+					value: 20, width: 255, height: 8, dotSize: 20, min: 0, max: 100, interval: 1,
 					disabled: false, show: true, speed: 0.3, reverse: false, lazy: false,
           tooltip: 'hover', piecewise: false
 				},
         sliderHigh: {
-					value: 20, width: 255, height: 8, dotSize: 20, min: 0, max: 255, interval: 1,
+					value: 20, width: 255, height: 8, dotSize: 20, min: 0, max: 100, interval: 1,
 					disabled: false, show: true, speed: 0.3, reverse: false, lazy: false,
 					tooltip: 'hover', piecewise: false
 				},
         sliderUndo: {
-					value: 20, width: 275, height: 8, dotSize: 20, min: 0, max: 255, interval: 1,
-					disabled: false, show: true, speed: 0.3, reverse: false, lazy: false,
-					tooltip: 'hover', piecewise: false
+					value: 0, width: 275, height: 8, dotSize: 20,
+          min: 0, max: 0, interval: 1, disabled: false,
+          show: true, speed: 0.3, reverse: false,
+          lazy: false, tooltip: 'hover', piecewise: false
 				}
       }
   	},
     mounted: function () {
-      var video  = document.getElementById('webcam');
-      var canvas = document.getElementById('canvas');
-      var gui, options, ctx, canvasWidth, canvasHeight, img_u8;
+      this.video  = document.getElementById('webcam');
+      this.canvas = document.getElementById('canvas');
       try {
-        compatibility.getUserMedia({video: true}, function(stream) {
+        compatibility.getUserMedia({video: true}, (stream) => {
           try {
-            video.src = compatibility.URL.createObjectURL(stream);
+            this.video.src = compatibility.URL.createObjectURL(stream);
           } catch (error) {
-            video.src = stream;
+            this.video.src = stream;
           }
-          video.addEventListener('canplay', canPlayListener, true);
+          this.video.addEventListener('canplay', this.canPlayListener, true);
         }, function (error) {
             document.getElementById('webcam').style.display = 'none';
             document.getElementById('canvas').style.display = 'none';
@@ -141,75 +169,84 @@
       }
       // var stat = new profiler();
 
-      function canPlayListener() {
-        video.removeEventListener('canplay', canPlayListener);
-        setTimeout(function() {
-          video.play();
-          demo_app();
-          compatibility.requestAnimationFrame(tick);
-        }, 500);
-      }
-      function demo_app() {
-          canvasWidth  = canvas.width;
-          canvasHeight = canvas.height;
-          ctx = canvas.getContext('2d');
-
-          ctx.fillStyle = "rgb(0,255,0)";
-          ctx.strokeStyle = "rgb(0,255,0)";
-
-          img_u8 = new jsfeat.matrix_t(640, 480, jsfeat.U8_t | jsfeat.C1_t);
-
-          // stat.add("grayscale");
-          // stat.add("gauss blur");
-          // stat.add("canny edge");
-      }
-
-      function tick() {
-          compatibility.requestAnimationFrame(tick);
-          // stat.new_frame();
-          if (video.readyState === video.HAVE_ENOUGH_DATA) {
-              ctx.drawImage(video, 0, 0, 640, 480);
-              var imageData = ctx.getImageData(0, 0, 640, 480);
-
-              // stat.start("grayscale");
-              jsfeat.imgproc.grayscale(imageData.data, img_u8.data);
-              // stat.stop("grayscale");
-
-              var kernel_size = 16;
-
-              // stat.start("gauss blur");
-              jsfeat.imgproc.gaussian_blur(img_u8, img_u8, kernel_size, 0);
-              // stat.stop("gauss blur");
-
-              var low_threshold  = 20;
-              var high_threshold = 50;
-
-              // stat.start("canny edge");
-              jsfeat.imgproc.canny(img_u8, img_u8, low_threshold|0, high_threshold|0);
-              // stat.stop("canny edge");
-
-              // render result back to canvas
-              var data_u32 = new Uint32Array(imageData.data.buffer);
-              var alpha = (0xff << 24);
-              var i = img_u8.cols*img_u8.rows, pix = 0;
-              while(--i >= 0) {
-                  pix = img_u8.data[i];
-                  data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
-              }
-
-              ctx.putImageData(imageData, 0, 0);
-
-              // document.getElementById('log').innerHTML = stat.log();
-          }
-      }
       this.$root.$on("navBarChange", function(to, from) {
         console.log(to,from);
         if(from.path == 'ZDemo') {
-          gui.close();
-          video.pause();
-          video.src=null;
+          this.video.pause();
+          this.video.src=null;
         }
       });
-    } // end mounted:
+    }, // end mounted:
+    methods: {
+      demo_app: function () {
+        this.ctx = this.canvas.getContext('2d');
+        this.ctx.fillStyle = "rgb(0,255,0)";
+        this.ctx.strokeStyle = "rgb(0,255,0)";
+        this.img_u8 = new jsfeat.matrix_t(640, 480, jsfeat.U8_t | jsfeat.C1_t);
+      },
+      processFrame: function () {
+        this.ctx.drawImage(this.video, 0, 0, 640, 480);
+        this.imageData = this.ctx.getImageData(0, 0, 640, 480);
+
+        jsfeat.imgproc.grayscale(this.imageData.data, this.img_u8.data);
+
+        var kernel_size = 16; // (radius+1)^2
+        jsfeat.imgproc.gaussian_blur(this.img_u8, this.img_u8, kernel_size, 0);
+
+        var low_threshold  = (this.lowThresh  > this.highThresh ?
+                              this.highThresh : this.lowThresh);
+        var high_threshold  = (this.lowThresh > this.highThresh ?
+                              this.lowThresh  : this.highThresh);
+        jsfeat.imgproc.canny(this.img_u8, this.img_u8,
+                             low_threshold|0, high_threshold|0);
+
+        // render result back to canvas
+        var data_u32 = new Uint32Array(this.imageData.data.buffer);
+        var alpha = (0xff << 24);
+        var i = this.img_u8.cols*this.img_u8.rows, pix = 0;
+        while(--i >= 0) {
+            pix = this.img_u8.data[i];
+            data_u32[i] = alpha | (pix << 16) | (pix << 8) | pix;
+        }
+        this.ctx.putImageData(this.imageData, 0, 0);
+      },
+      tick: function() {
+        if(this.mode != 'capturing') return;
+        compatibility.requestAnimationFrame(this.tick);
+        if (this.video.readyState === this.video.HAVE_ENOUGH_DATA)
+          this.processFrame();
+      },
+      canPlayListener: function () {
+        this.video.removeEventListener('canplay', this.canPlayListener);
+        setTimeout(() => {
+          this.video.play();
+          this.demo_app();
+          compatibility.requestAnimationFrame(this.tick);
+        }, 500);
+      },
+      lowChange: function(val) {
+        console.log("low:", val);
+        if(this.mode == "frozen") this.processFrame();
+      },
+      highChange: function(val) {
+        console.log("high:", val);
+        if(this.mode == "frozen") this.processFrame();
+      },
+      undoChange: function(val) {
+        console.log("undo:", val);
+      },
+      freezeClick: function() {
+        console.log("freeze click",this.mode, document.getElementById('freezeBtn'));
+        if(this.mode == 'capturing') {
+          this.mode = 'frozen';
+          this.freezeBtnText = "Restart";
+        }
+        else if(this.mode == 'frozen') {
+          this.mode = 'capturing';
+          this.freezeBtnText = "Freeze";
+          this.tick();
+        }
+      }
+    }
   }
 </script>
